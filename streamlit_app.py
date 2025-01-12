2c02159503033632e1a8e860ecb9f1631c587a6d
Naveen Chandra <83586615+NaveenChandra19@users.noreply.github.com>
2025-01-12 22:50:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smothie! :cup_with_straw:")
@@ -42,6 +43,4 @@ if ingredients_list:
  if time_to_insert:
     session.sql(my_insert_stmt).collect()
     st.success('''Your Smoothie is ordered, '''  + name_on_order + '''!''',  icon="✅");
-
-import requests
 
